07a727b0a7f0c566d29ef837b19e435e14ea8c04
Rodrigo S. Jauregui <rodrigojauregui@ici.org.uy>
2025-04-10 15:42:38 -0300

    Add model definition

---
+++ AspireEFCorePgSQLExample.CarsAPI/DAL/Models/Maker.cs
@@ -0,0 +1,17 @@
+﻿namespace AspireEFCorePgSQLExample.CarsAPI.DAL.Models;
+
+public class Maker
+{
+    public Guid Guid { get; set; }
+    public string Name { get; set; }
+    public string Country { get; set; }
+
+    public virtual IEnumerable<Car> Cars { get; set; } = Enumerable.Empty<Car>();
+
+    public Maker(Guid guid, string name, string country)
+    {
+        Guid = guid;
+        Name = name;
+        Country = country;
+    }
+}
